6dd3444a913c5f5f3f1f326885ef90acc52de6a7
Telenica <jejen560@gmx.de>
2025-10-16 17:04:58 +0200

    Server.py Update: Listener und Handler eingefügt

---
+++ Server.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+HOST = '127.0.0.1'
+PORT = 9999
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)  # Statuscode 200 OK
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Alles OK: 200 Success!")
+
+def start_server(host=HOST, port=PORT):
+    server_address = (host, port)
+    httpd = HTTPServer(server_address, MyHandler)
+    print(f"[SERVER] Läuft auf http://{host}:{port}/")
+    httpd.serve_forever()
+
+start_server()
+
+    
+    
