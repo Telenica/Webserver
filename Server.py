6f70c907b7f8b0159db81b99b34640022efcf5c3
Telenica <jejen560@gmx.de>
2025-10-17 15:03:11 +0200

    Server.py Update: um Kommentare ergänzt; Webserver.py erstellt um auf Webseite weiterzuleiten

--- Server.py
+++ Server.py
@@ -1,22 +1,21 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+# aus Host und Port zusammengesetzte Serveraddresse
 HOST = '127.0.0.1'
 PORT = 9999
 
-class MyHandler(BaseHTTPRequestHandler):
+class MyHandler(BaseHTTPRequestHandler): #Handler mit dem Server reagieren kann
     def do_GET(self):
-        self.send_response(200)  # Statuscode 200 OK
-        self.send_header("Content-type", "text/plain")
+        self.send_response(200) #200: success
+        self.send_header("Content-type", "text/plain") #Header einstellen
         self.end_headers()
-        self.wfile.write(b"Alles OK: 200 Success!")
+        self.wfile.write(b"Alles OK: 200 Success!") #Was auf Seite steht
 
+#Der Server der laufen soll
 def start_server(host=HOST, port=PORT):
-    server_address = (host, port)
-    httpd = HTTPServer(server_address, MyHandler)
+    server_address = (host, port) #Serveraddresse zusammensetzen
+    httpd = HTTPServer(server_address, MyHandler) #Handler auf Serveradresse werfen, damit Server antworten kann
     print(f"[SERVER] Läuft auf http://{host}:{port}/")
-    httpd.serve_forever()
+    httpd.serve_forever() #läuft bis man ihn stoppt
 
 start_server()
-
-    
-    
