484802eaf9d75030a9a6a7e6ad33278fb0375217
Telenica <jejen560@gmx.de>
2025-10-17 15:14:06 +0200

    Webserver Update: Kommentare hinzugefügt

--- Webserver.py
+++ Webserver.py
@@ -3,22 +3,22 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 HOST = '127.0.0.1'
 PORT = 9999
 
-class MyHandler(BaseHTTPRequestHandler):
+class MyHandler(BaseHTTPRequestHandler): #Handler mit dem Server reagieren kann
     def do_GET(self):
-        if self.path.startswith("/laurie"):
-            self.send_response(302)
-            self.send_header("Location", "https://tratt.net/laurie/")
+        if self.path.startswith("/laurie"): 
+            self.send_response(302) #302 temporäres weiterleiten auf Webseite
+            self.send_header("Location", "https://tratt.net/laurie/") #Seie die aufgerufen wird
             self.end_headers()
         else:
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_response(200) #200: success
+            self.send_header("Content-type", "text/plain") #Header einstellen
             self.end_headers()
-            self.wfile.write(b"Lokaler Server: Keine Weiterleitung.")
+            self.wfile.write(b"Lokaler Server: Keine Weiterleitung.") #Was auf Seite steht
 
 def start_server(host=HOST, port=PORT):
-    server_address = (host, port)
-    httpd = HTTPServer(server_address, MyHandler)
+    server_address = (host, port) #Serveraddresse zusammensetzen
+    httpd = HTTPServer(server_address, MyHandler) #Handler auf Serveradresse werfen, damit Server antworten kann
     print(f"[SERVER] Läuft auf http://{host}:{port}/")
-    httpd.serve_forever()
+    httpd.serve_forever() #läuft bis man ihn stoppt
 
 start_server()
